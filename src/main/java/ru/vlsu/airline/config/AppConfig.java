cfaeeb12052057e4993079d3f76a458a4b5fc6c5
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2024-02-17 18:12:34 +0300

    updated configuration

--- src/main/java/ru/vlsu/airline/config/AppConfig.java
+++ src/main/java/ru/vlsu/airline/config/AppConfig.java
@@ -2,11 +2,11 @@ package ru.vlsu.airline.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = "ru.vlsu.airline")
+@EnableJpaRepositories(basePackages = "ru.vlsu.airline.repositories")
 public class AppConfig {
 
 }
