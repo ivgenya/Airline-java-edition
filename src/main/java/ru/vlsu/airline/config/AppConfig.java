84eabeee398b3d055d90eeae8ee3538b12361968
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2024-03-02 19:38:46 +0300

    added annotations for scheduling and transactions

--- src/main/java/ru/vlsu/airline/config/AppConfig.java
+++ src/main/java/ru/vlsu/airline/config/AppConfig.java
@@ -3,12 +3,16 @@ package ru.vlsu.airline.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "ru.vlsu.airline")
 @EnableJpaRepositories(basePackages = "ru.vlsu.airline.repositories")
+@EnableTransactionManagement
+@EnableScheduling
 public class AppConfig {
 
 }
