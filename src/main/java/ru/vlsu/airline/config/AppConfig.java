5ed39f55d9dedc2bcb8eb60f73fc3272e7eb405f
SnezhG <stim.nuzhno@mail.ru>
2024-03-02 13:38:55 +0300

    added EnableWebMvc

--- src/main/java/ru/vlsu/airline/config/AppConfig.java
+++ src/main/java/ru/vlsu/airline/config/AppConfig.java
@@ -3,8 +3,10 @@ package ru.vlsu.airline.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "ru.vlsu.airline")
 @EnableJpaRepositories(basePackages = "ru.vlsu.airline.repositories")
 public class AppConfig {
