988def5967351815092bf45561d103dc785b5cb5
SnezhG <stim.nuzhno@mail.ru>
2024-03-02 13:38:02 +0300

    getAuthorities returns null and jwtfilter deleted for now

--- src/main/java/ru/vlsu/airline/entities/User.java
+++ src/main/java/ru/vlsu/airline/entities/User.java
@@ -78,7 +78,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return (Collection<? extends GrantedAuthority>) userRoles;
+        return null;
     }
 
     @Override
