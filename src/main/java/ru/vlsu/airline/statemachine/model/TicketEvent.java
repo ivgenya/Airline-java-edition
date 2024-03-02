82aff4d3eb29ddf87b5639aa149e8e6b7c4bfead
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2024-03-02 19:35:16 +0300

    added ticket states

---
+++ src/main/java/ru/vlsu/airline/statemachine/model/TicketEvent.java
@@ -0,0 +1,5 @@
+package ru.vlsu.airline.statemachine.model;
+
+public enum TicketEvent {
+    UNABLE, PAY, ANNUL, EXPIRE, CANCEL, USE
+}
---
+++ src/main/java/ru/vlsu/airline/statemachine/model/TicketState.java
@@ -0,0 +1,5 @@
+package ru.vlsu.airline.statemachine.model;
+
+public enum TicketState {
+    UNPAID, PAID, EXPIRED, UNABLED_TO_PAY, USED, ANNULLED, CANCELLED
+}
