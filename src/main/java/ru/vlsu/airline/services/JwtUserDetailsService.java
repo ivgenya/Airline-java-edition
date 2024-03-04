fb2b072d47a229e4909b426fccbb1a1997688fff
SnezhG <stim.nuzhno@mail.ru>
2024-03-04 20:40:17 +0300

    added jwtuserdetailsservice

---
+++ src/main/java/ru/vlsu/airline/services/JwtUserDetailsService.java
@@ -0,0 +1,26 @@
+package ru.vlsu.airline.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.vlsu.airline.entities.User;
+import ru.vlsu.airline.repositories.UserRepository;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if(user != null){
+            return user;
+        }else{
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+}
