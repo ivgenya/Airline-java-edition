41e72189b723a62e69b5a1ccd95448e2ee2d35bd
SnezhG <stim.nuzhno@mail.ru>
2024-03-04 20:40:50 +0300

    added security initializer (registers security chain for app)

---
+++ src/main/java/ru/vlsu/airline/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package ru.vlsu.airline.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+// регистрирует securitychain
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
