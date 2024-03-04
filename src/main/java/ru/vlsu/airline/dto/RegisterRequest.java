b5fc2612fd1eae0e8bcb6874e7f434170c3492dc
SnezhG <stim.nuzhno@mail.ru>
2024-03-04 20:39:38 +0300

    added request and response models for auth

---
+++ src/main/java/ru/vlsu/airline/dto/RegisterRequest.java
@@ -0,0 +1,13 @@
+package ru.vlsu.airline.dto;
+
+public class RegisterRequest {
+
+    private String email;
+    private String password;
+
+    public String getEmail(){return email;}
+    public void setEmail(String email){this.email = email;}
+
+    public String getPassword(){return password;}
+    public void setPassword(String password){this.password = password;}
+}
