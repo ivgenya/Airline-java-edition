6546c30599efd5c3f6bf5d174885f075bce8ae7f
SnezhG <stim.nuzhno@mail.ru>
2024-03-04 20:36:34 +0300

    added findByEmail

--- src/main/java/ru/vlsu/airline/repositories/UserRepository.java
+++ src/main/java/ru/vlsu/airline/repositories/UserRepository.java
@@ -1,10 +1,12 @@
 package ru.vlsu.airline.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.vlsu.airline.entities.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email);
+    User findByEmail(String email);
 }
