ca56695234d668cf4ad5eaebba84108c413854a4
SnezhG <stim.nuzhno@mail.ru>
2024-03-31 16:15:08 +0300

    added roledto

---
+++ src/main/java/ru/vlsu/airline/dto/RoleDTO.java
@@ -0,0 +1,10 @@
+package ru.vlsu.airline.dto;
+
+public class RoleDTO {
+
+    private String roleName;
+
+    public String getRoleName(){return roleName;}
+
+    public void setRoleName(String roleName){this.roleName = roleName;}
+}
