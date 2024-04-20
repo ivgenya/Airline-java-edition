e2b88ebe51968109509aa1bd61d2dcd6cae266ac
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2024-04-20 12:01:57 +0300

    Allowed all methods

--- src/main/java/ru/vlsu/airline/config/CorsConfig.java
+++ src/main/java/ru/vlsu/airline/config/CorsConfig.java
@@ -11,6 +11,7 @@ public class CorsConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("*");
     }
 }
